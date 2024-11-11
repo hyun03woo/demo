9a6d71e2d6dd9e33da3330193433b22abcf5e9bd
313 <313@DESKTOP-5QB8UV0>
2024-11-11 15:58:44 +0900

    abcd

---
+++ demo/src/test/java/gwangju_university/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package gwangju_university.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
